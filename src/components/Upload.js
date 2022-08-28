b9e73603a174c1b5f119206663c8060ea0d6a064
suleun <teaaa030@gmail.com>
2022-08-29 03:30:05 +0900

    create

--- src/components/Upload.js
+++ src/components/Upload.js
@@ -1,12 +1,80 @@
 import React, { useState } from 'react'
 
+import { useNavigate } from 'react-router-dom';
+
 import { Link } from 'react-router-dom'
 import logo from '../img/logo.png'
 
 import upload from '../img/upload.jpg'
 
 
-function Upload() {
+  function Upload() {
+
+    
+    const navigate = useNavigate();
+
+    const [title, setTitle] = useState('');
+    const [SContent, setSContent] = useState('');
+    const [TContent, setTContent] = useState('');
+    const [author, setAuthor] = useState('');
+    const [toStudy, setToStudy] = useState('');
+    const [toPrac, setToPrac] = useState('');
+    const [topic, setTopic] = useState('');
+
+    const onSubmitForm = async (e) => {
+      e.preventDefault();
+      
+      const newPost = {  
+        title: title,
+        SContent: SContent,
+        TContent: TContent,
+        author: author,
+        toStudy: toStudy,
+        toPrac: toPrac,
+        topic: topic
+      };
+      
+      const submitFormResponse = await (await fetch('http://localhost:3001/posts', {
+          method: 'POST',
+          headers: {
+              'Content-Type': 'application/json',
+          },
+          body: JSON.stringify(newPost),
+      })).json();
+
+      navigate(`/${submitFormResponse.id}`);
+      alert("등록 완료!")
+      window.location.replace("/");
+  };
+
+  const onChangeTitle = (e) => {
+    setTitle(e.target.value);
+  };
+
+  const onChangeSContent = (e) => {
+    setSContent(e.target.value);
+  };
+
+  const onChangeTContent = (e) => {
+    setTContent(e.target.value);
+  };
+
+  const onChangeAuthor  = (e) => {
+    setAuthor(e.target.value);
+  };
+
+  const onChangeToStudy = (e) => {
+    setToStudy(e.target.value);
+  };
+
+  const onChangeToPrac = (e) => {
+    setToPrac(e.target.value);
+  };
+
+  const onChangeTopic = (e) => {
+    setTopic(e.target.value);
+  };
+
 
   return (
     <div class="">
@@ -33,56 +101,58 @@ function Upload() {
       </header>
         
       <div class="flex justify-center ">
-        
-        <div class="py-6 px-6 bg-green-300 rounded-2xl w-2/3">
-          
-          <div class="space-y-4">
-            <div class="flex justify-evenly space-x-5">
-              <input type="text" placeholder="Title" class="text-sm py-3 px-4 rounded-lg w-1/2 outline-none" />
-              <input type="text" placeholder="Your Name" class="text-sm py-3 px-4 rounded-lg w-1/2 outline-none" />
-            </div>
-
-            <div>
-              <div class=" flex justify-left space-x-auto my-3">
-                <label for="underline_select" class="text-left font-bold mr-3 p-2">  to Study : </label>
-                  <select id="underline_select" class=" text-sm rounded-2xl p-2 border-0 border-b-2 border-gray-200">
-                      <option selected="">Choose a country</option>
-                      <option value="ja">Japan</option>
-                      <option value="ko">Korea</option>
-                  </select>
-              </div>
-              <textarea id="content" rows="6" class="block p-5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg "  maxlength='1000' placeholder="Your message..."></textarea>
-              
-            </div>
+        <form onSubmit={onSubmitForm}>
+          <div class="py-6 px-6 bg-green-300 rounded-2xl w-full">
             
-            <div>
-              <div class=" flex justify-left space-x-auto my-3">
-                <label for="underline_select" class="text-left font-bold mr-3 p-2">  to practice typing : </label>
-                  <select id="underline_select" class=" text-sm rounded-2xl p-2 border-0 border-b-2 border-gray-200">
-                      <option selected="">Choose a country</option>
-                      <option value="ja">Japan</option>
-                      <option value="ko">Korea</option>
-                  </select>
+            <div class="space-y-4">
+              <div class="flex justify-evenly space-x-5">
+                <input type="text" placeholder="Title" id='title' onChange={onChangeTitle} value={title} class="text-sm py-3 px-4 rounded-lg w-1/2 outline-none" />
+                <input type="text" placeholder="Your Name" id='author' onChange={onChangeAuthor} value={author} class="text-sm py-3 px-4 rounded-lg w-1/2 outline-none" />
               </div>
-              <textarea id="message" rows="6" class="block p-5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg" maxlength='1000' placeholder="Your message..."></textarea>
+
+              <div>
+                <div class=" flex justify-left space-x-auto my-3">
+                  <label for="underline_select" class="text-left font-bold mr-3 p-2">  to Study : </label>
+                    <select id='toStudy' onChange={onChangeToStudy} value={toStudy} class=" text-sm rounded-2xl p-2 border-0 border-b-2 border-gray-200">
+                        <option selected="">Choose a country</option>
+                        <option value="Japan">Japan</option>
+                        <option value="Korea">Korea</option>
+                    </select>
+                </div>
+                <textarea id='SContent' onChange={onChangeSContent} value={SContent} rows="6" class="block p-5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg "  maxlength='1000' placeholder="Your message..."></textarea>
+                
+              </div>
               
+              <div>
+                <div class=" flex justify-left space-x-auto my-3">
+                  <label for="underline_select" class="text-left font-bold mr-3 p-2">  to practice typing : </label>
+                    <select id='toPrac' onChange={onChangeToPrac} value={toPrac} class=" text-sm rounded-2xl p-2 border-0 border-b-2 border-gray-200">
+                        <option selected="">Choose a country</option>
+                        <option value="Japan">Japan</option>
+                        <option value="Korea">Korea</option>
+                    </select>
+                </div>
+                <textarea id='TContent' onChange={onChangeTContent} value={TContent} rows="6" class="block p-5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg" maxlength='1000' placeholder="Your message..."></textarea>
+                
+              </div>
+              
+
             </div>
-            
+
+            <div class="text-left font-bold mt-5">
+              Topic : 
+              <br/>
+              <textarea id='topic' onChange={onChangeTopic} value={topic} rows="1" class="block p-5 mt-3 w-full text-sm text-gray-900 bg-gray-50 rounded-lg" maxlength='1000' placeholder="Your message..."></textarea>
+                
+            </div>
+
+            <div class="text-center mt-6 mx-auto">
+              <button  type="submit" class="py-3 px-3 text-xl text-white bg-purple-400 rounded-2xl">Create</button>
+            </div>
 
           </div>
+        </form>
 
-          <div class="text-left font-bold mt-5">
-            Topic : 
-            <br/>
-            <textarea id="message" rows="1" class="block p-5 mt-3 w-full text-sm text-gray-900 bg-gray-50 rounded-lg" maxlength='1000' placeholder="Your message..."></textarea>
-              
-          </div>
-
-          <div class="text-center mt-6 mx-auto">
-            <button class="py-3 px-3 text-xl text-white bg-purple-400 rounded-2xl">Create</button>
-          </div>
-
-        </div>
       </div>
 
     </div>
